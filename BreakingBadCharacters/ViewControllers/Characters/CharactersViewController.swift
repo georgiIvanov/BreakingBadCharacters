3e21624ad867e20ad993497ed15fa25774d57ec7
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2020-07-31 16:56:57 +0300

    Characters view controller declaration, setup project root folders

---
+++ BreakingBadCharacters/ViewControllers/Characters/CharactersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharactersViewController.swift
+//  BreakingBadCharacters
+//
+//  Created by Voro on 31.07.20.
+//  Copyright © 2020 GeorgiIvanov. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
