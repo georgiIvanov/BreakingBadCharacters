2aaba3de5d5bd0eea6f18d2177cd8e8aef9d8966
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2020-08-01 17:29:22 +0300

    Displaying characters in a carousel

--- BreakingBadCharacters/ViewControllers/Characters/CharactersViewController.swift
+++ BreakingBadCharacters/ViewControllers/Characters/CharactersViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 import RxSwift
 
 class CharactersViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var viewModel: CharactersViewModelProtocol!
+    var characters: [BreakingBadCharacter] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        
         viewModel.fetchCharacters()
+        
+        viewModel.characters.drive(onNext: { [weak self] (characters) in
+            self?.characters = characters
+        }).disposed(by: disposeBag)
     }
+    
+    func setupUI() {
+        let layout = ZoomAndSnapFlowLayout()
+        let sizePercent: CGFloat = 0.6
+        let verticalInset: CGFloat = 20
+        let collectionSize = view.frame.size
+        layout.itemSize = CGSize(width: collectionSize.width * sizePercent,
+                                 height: collectionSize.height * sizePercent - verticalInset)
+        
+        collectionView.collectionViewLayout = layout
+        collectionView.contentInsetAdjustmentBehavior = .always
+    }
+}
+
+// MARK: - Collection View Datasource
+
+extension CharactersViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "characterCell",
+                                                            for: indexPath) as? CharacterCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.setup(characters[indexPath.item])
+        
+        return cell
+    }
+    
+    
 }
