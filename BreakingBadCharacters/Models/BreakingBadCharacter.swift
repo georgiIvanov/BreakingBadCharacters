2de44826b1e187c7aa712a2a50eefc438e5da069
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2020-08-01 13:40:20 +0300

    BreakingBadCharacter model

---
+++ BreakingBadCharacters/Models/BreakingBadCharacter.swift
@@ -0,0 +1,23 @@
+//
+//  BreakingBadCharacter.swift
+//  BreakingBadCharacters
+//
+//  Created by Voro on 1.08.20.
+//  Copyright © 2020 GeorgiIvanov. All rights reserved.
+//
+
+import Foundation
+
+struct BreakingBadCharacter {
+    let charId: Int
+    let name: String
+    let birthday: String
+    let occupation: [String]
+    let img: String
+    let status: String
+    let nickname: String
+    let appearance: [Int]
+    let portrayed: String
+    let category: String
+    let betterCallSaulAppearance: [Int]
+}
